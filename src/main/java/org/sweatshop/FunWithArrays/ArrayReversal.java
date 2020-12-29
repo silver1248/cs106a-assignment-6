e8cb6a3c3271b20f6dba6a9307abd900ec100dc3
d <silver1248@users.noreply.github.com>
2020-12-29 17:38:59 -0500

    made a thing to reverse elements in an array and arrays themselves

--- src/main/java/org/sweatshop/FunWithArrays/ArrayReversal.java
+++ src/main/java/org/sweatshop/FunWithArrays/ArrayReversal.java
@@ -2,13 +2,33 @@ package org.sweatshop.FunWithArrays;
 
 public class ArrayReversal {
 
-	public static int[] reversal(int[] in) {
-		int length = in.length;
-		for(int i = 0; i < length / 2; i++) {
-			int holder = in[length - i];
-			in[length - i] = in[i];
-			in[i] = holder;
-		}
-		return in;
-	}
+    public static int[] elementReversal(int[] in) {
+        if (in == null || in.length == 0) {
+            return new int[0];
+        } else {
+            int length = in.length;
+            int[] reversedIn = new int[length];
+            for (int i = 0; i < (length / 2)+1; i++) {
+                int iComplement = length - (i + 1);
+                reversedIn[i] = in[iComplement];
+                reversedIn[iComplement] = in[i];
+            }
+            return reversedIn;
+        }
+    }
+    
+    public static int[][] arrayReversal(int[][] in) {
+        if (in == null || in.length == 0) {
+            return new int[0][0];
+        } else {
+            int length = in.length;
+            int[][] reversedIn = new int[length][];
+            for (int i = 0; i < (length / 2)+1; i++) {
+                int iComplement = length - (i + 1);
+                reversedIn[i] = in[iComplement];
+                reversedIn[iComplement] = in[i];
+            }
+            return reversedIn;
+        }
+    }
 }
--- src/test/java/org/sweatshop/FunWithArrays/ArrayReversalTest.java
+++ src/test/java/org/sweatshop/FunWithArrays/ArrayReversalTest.java
@@ -5,12 +5,11 @@ import static org.testng.Assert.assertEquals;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-
 public class ArrayReversalTest {
 
     @Test(dataProvider="turnAroundTestDP")
     public void turnAroundTest(int[] in, int[] expected) {
-    	int[] actual = ArrayReversal.reversal(in);
+    	int[] actual = ArrayReversal.elementReversal(in);
         assertEquals(actual, expected);
     }
 
@@ -25,6 +24,29 @@ public class ArrayReversalTest {
             {new int[]{5,4,3,2,1}, new int[]{1,2,3,4,5}},
             {new int[]{8,7,4,9,3}, new int[]{3,9,4,7,8}},
             {new int[]{18,19,20,21,22}, new int[]{22,21,20,19,18}},
+            {new int[][]{{1,2,3,4}, {5,6,7,8}}, new int[][]{{5,6,7,8}, {1,2,3,4}}},
+        };
+    }
+    
+    @Test
+    public void instanceTest() {
+        new ArrayReversal();
+    }
+    
+    @Test(dataProvider="turnAroundArraysTestDP")
+    public void turnAroundArraysTest(int[][] in, int[][] expected) {
+        int[][] actual = ArrayReversal.arrayReversal(in);
+        assertEquals(actual, expected);
+    }
+    
+    @DataProvider
+    Object[][] turnAroundArraysTestDP() {
+        return new Object[][] {
+            {new int[][]{{}}, new int[][]{{}}},
+            {new int[0][], new int[0][]},
+            {new int[][]{{1,2}, {3,4}}, new int[][]{{3,4}, {1,2}}},
+            {new int[][]{{1,2,3,4}}, new int[][]{{1,2,3,4}}},
+            {new int[][]{{1,2,3,4}, {5,6,7,8}}, new int[][]{{5,6,7,8}, {1,2,3,4}}},
         };
     }
 }
