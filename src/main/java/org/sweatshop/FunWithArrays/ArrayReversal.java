e407409f0da1444536b395b5fcaa68bbbdf42707
d <silver1248@users.noreply.github.com>
2020-12-30 17:05:17 -0500

    made a function that reverses a list of generics

--- src/main/java/org/sweatshop/FunWithArrays/ArrayReversal.java
+++ src/main/java/org/sweatshop/FunWithArrays/ArrayReversal.java
@@ -1,5 +1,9 @@
 package org.sweatshop.FunWithArrays;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class ArrayReversal {
 
     public static int[] elementReversal(int[] in) {
@@ -27,6 +31,21 @@ public class ArrayReversal {
                 int iComplement = length - (i + 1);
                 reversedIn[i] = in[iComplement];
                 reversedIn[iComplement] = in[i];
+            }
+            return reversedIn;
+        }
+    }
+    
+    public static <T> List<T> listReversal(List<T> in) {
+        if (in == null || in.size() == 0) {
+            return new LinkedList<T>();
+        } else {
+            int length = in.size();
+            List<T> reversedIn = new ArrayList<T>(length);
+            for (int i = 0; i < (length / 2)+1; i++) {
+                int iComplement = length - (i + 1);
+                reversedIn.set(i, in.get(iComplement));
+                reversedIn.set(iComplement, in.get(i));
             }
             return reversedIn;
         }
