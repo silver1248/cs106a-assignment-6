f0e24a9a95374dc0b7273b315b462f7fcd9ed5c1
Daniel Abraham <dabraham@sweatshop.org>
2021-07-21 20:08:51 -0400

    reset binary tree stuff to more where I was aiming

--- src/main/java/BinaryTree/Node.java
+++ src/main/java/BinaryTree/Node.java
@@ -4,32 +4,13 @@ import io.vavr.control.Option;
 import lombok.Value;
 
 @Value
-public class Node {
-    static Option<Node> left;
-    static Option<Node> right;
-    int value;
-    
-    public Node(Option<Node> left, Option<Node> right, int value) {
-        Node.left = left;
-        Node.right = right;
-        this.value = value;
+public class Node<T> {
+    T value;
+    Option<Node<T>> left;
+    Option<Node<T>> right;
+
+    public static <T> Node<T> newNode(T t) {
+        return new Node<T>(t, Option.none(), Option.none());
     }
-    
-    public Node getLeft() {
-        return left.get();
-    }
-    
-    public Option<Node> setLeft(Node leftVal) {
-        left = Option.some(leftVal);
-        return left;
-    }
-    
-    public Node getRight() {
-        return right.get();
-    }
-    
-    public Option<Node> setRight(Node rightVal) {
-        right = Option.some(rightVal);
-        return right;
-    }
+
 }
--- src/main/java/BinaryTree/Tree.java
+++ src/main/java/BinaryTree/Tree.java
@@ -1,27 +1,41 @@
 package BinaryTree;
 
 import io.vavr.control.Option;
+import lombok.Value;
+import lombok.experimental.NonFinal;
 
-public class Tree {
-    static Node topNode;
-    static Node nextNode;
+@Value
+public class Tree<T> {
+    @NonFinal private Option<Node<T>> root = Option.none();
 
-    public Tree (Node node) {
-        Tree.topNode = node;
-        Tree.nextNode = topNode;
+    public Tree<T> add(T t) {
+        Node<T> node = Node.newNode(t);
+
+        /* here you're going to hunt down the correct place in the tree, then recreate that bit with this node at the bottom,
+           all the way back up to the root
+
+           So if we have this tree
+
+                    3
+                  /   \
+                 2     17
+                      /
+                    10
+
+           then we add 20, we'll work our way to the 17 node, and create a new 17 node with 20 on the right, then a new
+           3 node with the new 17 on the right, then that's the root, so replace the root of the tree.
+
+           It returns "this" so that you can do
+                tree.add(3).add(2).add(17).add(10).add(20);
+
+
+           as a hint, you recursed your way down, you can replace while recursing back up
+
+           */
+
+
+
+        return this;
     }
 
-    public static void add (Node node) {
-        if (nextNode.getValue() > node.getValue()) {
-            if (Option.none().equals(nextNode.getLeft().getValue())) {
-                nextNode = nextNode.getLeft();
-            }
-        } else if (nextNode.getValue() < node.getValue()) {
-            if (Option.none().equals(nextNode.getRight().getValue())) {
-                nextNode = nextNode.getRight();
-            }
-        }
-        add(nextNode);
-        nextNode = topNode;
-    }
 }
